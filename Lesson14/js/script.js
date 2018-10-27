06ea8f6ff6a29bec52bc93112e65e88279d0a8c3
Anastas Mironov <thebreakhack@gmail.com>
2018-10-28 04:30:17 +0900

    new animate version slide

--- Lesson14/js/script.js
+++ Lesson14/js/script.js
@@ -1,12 +1,25 @@
 $(document).ready(() => {
 
     $('.main_btn, .main_btna, [href="#sheldure"]').on('click', () => {
-        $('.modal').slideDown(1000);
+
+        // update version 2
+        $('.modal').css({'display': 'block', 'top': '-100%'});
+        $('.modal').animate({ top: '0px'}, 1000);
+
+        // previous version 1
+        // $('.modal').slideDown(1000);
+
         $('.overlay').fadeTo(1000, 1);
     });
 
     $('.close').on('click', () => {
-        $('.modal').slideUp(1000);
+
+        // update version 2
+        $('.modal').animate({ top: '-100%'}, 1000);
+        $('.modal').css({'display': 'block'});
+
+        // previous version 1
+        // $('.modal').slideUp(1000);
         $('.overlay').fadeOut(1000);
     });
     
